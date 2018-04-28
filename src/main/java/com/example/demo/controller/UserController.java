6c4b24e44316f7bace067f9ececbaaadeaf1c2c0
尹雄峰 <790009351@qq.com>
2018-04-28 11:57:59 +0800

    Redis测试

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.example.demo.config.RedisConfig;
 import com.example.demo.model.Person;
 import com.example.demo.service.UserService;
@@ -9,6 +10,8 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
 
 /**
  * Created by yinxf on 2018/4/18.
@@ -48,6 +51,38 @@ public class UserController {
         util.set(name, "张思");
 
         String n=util.get(name).toString();
+
+        userService.testRedis(util);
+
+        System.out.println(util.get("p1"));
+
+        Person person=(Person) util.get("p1");
+        System.out.println(person.getId()+"-"+person.getName());
+
+        System.out.println(util.lGet("persons",0,5));
+        Iterator iter=util.lGet("persons",0,5).iterator();
+        while (iter.hasNext()){
+            Person p=(Person) iter.next();
+            System.out.println(p.getId()+"--"+p.getName()+"--"+p.getAge());
+        }
+
+        Map<String,Object> map=new HashMap<>();
+        map.put("name","德玛西亚");
+        map.put("area","班德尔城");
+        util.hmset("hero",map);
+
+        System.out.println(util.hget("hero","name"));
+        System.out.println(util.hmget("hero"));
+        Map hero=util.hmget("hero");
+        System.out.println(hero.get("name")+"-"+hero.get("area"));
+
+
+        util.sSet("work","射手","刺客","战士","法师","坦克");
+        System.out.println(util.sGet("work"));
+        Iterator work=util.sGet("work").iterator();
+        while (work.hasNext()){
+            System.out.println(work.next());
+        }
 
         return n;
 
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -2,10 +2,14 @@ package com.example.demo.service;
 
 import com.example.demo.mapper.UserMapper;
 import com.example.demo.model.Person;
+import com.example.demo.utils.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by yinxf on 2018/4/18.
@@ -20,4 +24,32 @@ public class UserService implements UserMapper{
         return userMapper.selectUser(id);
     }
 
+    public void testRedis(RedisUtil util) {
+        List<Person> list=new ArrayList<>();
+        Person p1= new Person();
+        p1.setId(2);
+        p1.setName("可可西");
+        p1.setAge(25);
+        util.set("p1",p1);
+
+        Person p2= new Person();
+        p2.setId(3);
+        p2.setName("亚索");
+        p2.setAge(23);
+
+        Person p3= new Person();
+        p3.setId(4);
+        p3.setName("德莱文");
+        p3.setAge(27);
+
+        list.add(p1);
+        list.add(p2);
+        list.add(p3);
+
+        util.del("persons");
+        util.lSet("persons",p1);
+        util.lSet("persons",p2);
+        util.lSet("persons",p3);
+
+    }
 }
